ec6c1c324816b7834e8c6fde86ed1de47ebeec82
AnirbanNath-dev <anirbaniitian@gmail.com>
2024-01-14 22:44:48 +0530

    Updated the quiz component

--- src/pages/Quiz.tsx
+++ src/pages/Quiz.tsx
@@ -1,23 +1,42 @@
-import React from 'react'
 
 function Quiz() {
   return (
-    <div className='w-screen h-screen flex flex-col items-center p-10'>
-        <div className='h-fit flex gap-10 items-center m-40'>
-          <div className='h-12 w-12 bg-black text-white rounded-full text-2xl flex justify-center items-center'>1</div>
-          <span className='text-2xl'>What was the name of the </span>
+    <div className='w-screen h-screen flex flex-col items-center'>
+      <div className='h-fit flex gap-10 items-center my-40'>
+        <div className='h-12 w-12 bg-black text-white rounded-full text-2xl flex justify-center items-center'>1</div>
+        <span className='text-2xl'>What was the name of the </span>
+      </div>
+
+      <div className='flex flex-col gap-8 w-1/3'>
+        <div className='flex justify-between'>
+
+          <div className='flex items-center cursor-pointer gap-4'>
+            <div className='h-10 w-10 bg-black text-white rounded-full text-2xl flex justify-center items-center'>A</div>
+            <span className='text-2xl'>Anirban</span>
+          </div>
+          <div className='flex items-center cursor-pointer gap-4'>
+            <div className='h-10 w-10 bg-black text-white rounded-full text-2xl flex justify-center items-center'>B</div>
+            <span className='text-2xl'>Anirban</span>
+          </div>
+
         </div>
 
-        <div>
-          <div>
-            <div className='h-12 w-12 bg-black text-white rounded-full text-2xl flex justify-center items-center'>A</div>
 
-          </div>
-          <div>
-            <div className='h-12 w-12 bg-black text-white rounded-full text-2xl flex justify-center items-center'>B</div>
 
+        <div className='flex justify-between'>
+
+          <div className='flex items-center cursor-pointer gap-4'>
+            <div className='h-10 w-10 bg-black text-white rounded-full text-2xl flex justify-center items-center'>C</div>
+            <span className='text-2xl'>Anirban</span>
           </div>
+          <div className='flex items-center cursor-pointer gap-4'>
+            <div className='h-10 w-10 bg-black text-white rounded-full text-2xl flex justify-center items-center'>D</div>
+            <span className='text-2xl'>Anirban</span>
+          </div>
+
         </div>
+
+      </div>
     </div>
   )
 }
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -4,7 +4,8 @@ import auth from "../types/authType";
 const limit = useSelector<auth>(state =>  state.limit)
 
 
-// const TOKEN: string = import.meta.env.VITE_QUIZAPP_TOKEN;  //not needed for this api 
+//not needed for this api 
+// const TOKEN: string = import.meta.env.VITE_QUIZAPP_TOKEN;  
 
 
 const api = `https://opentdb.com/api.php?amount=${limit}&type=multiple`
